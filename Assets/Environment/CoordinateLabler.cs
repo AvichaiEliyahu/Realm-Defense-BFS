9bb901a907a1124937da6d8b19493081eceba32a
AvichaiEliyahu <avihayboom@gmail.com>
2021-04-22 19:32:46 +0300

    added bfs path finding

--- Assets/Environment/CoordinateLabler.cs
+++ Assets/Environment/CoordinateLabler.cs
@@ -10,16 +10,18 @@ using System;
 public class CoordinateLabler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
-    [SerializeField] Color blockedColor = Color.blue;
+    [SerializeField] Color blockedColor = Color.grey;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f,0.5f,0);// orange
 
     TextMeshPro lable;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         lable = GetComponent<TextMeshPro>();
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();  
     }
     void Update()
@@ -43,16 +45,30 @@ public class CoordinateLabler : MonoBehaviour
 
     private void SetLableColor()
     {
-        if (waypoint.IsPlaceable)
-            lable.color = defaultColor;
-        else
+        if (gridManager == null)
+            return;
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null)
+            return;
+
+        if (!node.isWalkable)
             lable.color = blockedColor;
+        else if (node.isPath)
+            lable.color = pathColor;
+        else if (node.isExplored)
+            lable.color = exploredColor;
+        else
+            lable.color = defaultColor;
     }
 
     void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x/UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z/UnityEditor.EditorSnapSettings.move.z);
+        if (gridManager == null)
+            return;
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x/gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z/gridManager.UnityGridSize);
         lable.text = coordinates.x + "," + coordinates.y;
     }
 
